a4b9aa3f4396de42096e12fffe059140bcfa1874
noahlee5869 <60622096+noahlee5869@users.noreply.github.com>
2024-05-15 03:57:44 +0000

    Week 03 re-final commit

--- week03/code/SetsAndMapsTester.cs
+++ week03/code/SetsAndMapsTester.cs
@@ -285,7 +285,7 @@ public static class SetsAndMapsTester {
     /// 
     /// </summary>
  
-}   private static void EarthquakeDailySummary() {
+   private static void EarthquakeDailySummary() {
 
         const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
         
@@ -302,4 +302,4 @@ public static class SetsAndMapsTester {
         // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to print out each place a earthquake has happened today and its magitude.
-}
+}}
