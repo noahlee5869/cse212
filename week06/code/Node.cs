ce6c4381ef80c02b4f3ebd0b33a72b761b6e536a
noahlee5869 <60622096+noahlee5869@users.noreply.github.com>
2024-06-01 17:40:06 +0000

    Problems 1-5 Final Commit

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -8,6 +8,9 @@ public class Node {
     }
 
     public void Insert(int value) {
+        if(value == Data){
+            return;
+        }
         if (value < Data) {
             // Insert to the left
             if (Left is null)
@@ -25,12 +28,30 @@ public class Node {
     }
 
     public bool Contains(int value) {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data) {
+            return true;
+        }
+        if (value < Data) {
+            if (Left is null) {
+                return false;
+            }
+            else {
+                return Left.Contains(value);
+            }
+        }
+        else {
+            if (Right is null) {
+                return false;
+            }
+            else {
+                return Right.Contains(value);
+            }
+        }
     }
 
     public int GetHeight() {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
--- week06/code/TreesTester.cs
+++ week06/code/TreesTester.cs
@@ -97,6 +97,12 @@ public static class TreesTester {
     /// <param name="last">the last index in the sortedNumbers to insert</param>
     /// <param name="bst">the BinarySearchTree in which to insert the values</param>
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst) {
-        // TODO Start Problem 5
+        if (first > last) {
+            return; // Base case
+        }
+        int mid = (first + last) / 2;
+        bst.Insert(sortedNumbers[mid]);
+        InsertMiddle(sortedNumbers, first, mid - 1, bst);
+        InsertMiddle(sortedNumbers, mid + 1, last, bst);
     }
 }
